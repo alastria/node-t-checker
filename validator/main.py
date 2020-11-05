71070c2f72d6c7717d01813109d0596569cfb628
romerox3 <romerox33@gmail.com>
2020-11-05 14:39:20 +0100

    pushing

--- validator/main.py
+++ validator/main.py
@@ -50,9 +50,7 @@ def validate():
     print(f"NODE INFO-> {node_info}")
 
     node_validator = NodeValidator(node_info)
-
     node_validator.use_enode_request_config(enode_config)
-
     output: ValidatorOutput = node_validator.get_validation()
 
     print(f"OUTPUT-> {output}")
